a4df9402fd4c104b4463ac3b93d5ca1da7de391f
diegofesh <diegofesh@gmail.com>
2024-01-13 23:22:56 -0500

    modifued controller and add model class

---
+++ src/main/java/com/dfesh/form/springdemo/student/model/Student.java
@@ -0,0 +1,32 @@
+package com.dfesh.form.springdemo.student.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Student {
+
+    private String firstname;
+
+    private String lastname;
+
+    private LocalDate dateOfBirth;
+
+    private String email;
+
+    private String age;
+
+    public Student() {
+    }
+
+    public Student(String firstname, String lastname, LocalDate dateOfBirth, String email, String age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.age = age;
+    }
+}
