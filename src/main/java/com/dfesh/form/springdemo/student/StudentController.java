ea7fd95d21945ce6d7bd1e44b3002657ff792cfd
diegofesh <diegofesh@gmail.com>
2024-01-15 16:22:48 -0500

    explained aplicationContext dependence injections

--- src/main/java/com/dfesh/form/springdemo/student/StudentController.java
+++ src/main/java/com/dfesh/form/springdemo/student/StudentController.java
@@ -1,6 +1,7 @@
 package com.dfesh.form.springdemo.student;
 
 import com.dfesh.form.springdemo.student.model.Student.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,11 @@ import java.util.List;
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
-    private final StudentService service = new StudentService();
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
 
     @GetMapping
     public List<Student> findAllStudents() {
