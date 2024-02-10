15ce311f612a98b86ce1d1a9a436fbc9b6b4f788
diegofesh <diegofesh@gmail.com>
2024-02-10 00:09:16 -0500

    optimize import

--- src/main/java/com/dfesh/form/springdemo/student/model/Student/Student.java
+++ src/main/java/com/dfesh/form/springdemo/student/model/Student/Student.java
@@ -1,6 +1,12 @@
 package com.dfesh.form.springdemo.student.model.Student;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 
